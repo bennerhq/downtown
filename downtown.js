eb371c867658fc1fa8cbfa65979a438170b0cae2
Jens Kaas Benner <jens@bennerhq.com>
2012-12-25 21:39:54 +0100

    Update downtown.js

--- downtown.js
+++ downtown.js
@@ -107,30 +107,41 @@ function Downtown(id_field, id_score, id_hi) {
 			var h = this.field_height - space_min - 
 		       		Math.floor(Math.random() * space_max / BOX_HEIGHT) * BOX_HEIGHT;
 		
-			var div = document.createElement("div");
-			div.id = "b" + this.count;
-			div.style.left = (this.count * BOX_WIDTH) + "px";
-			div.style.top = h + "px";
-			div.style.width = (BOX_WIDTH - 4) + "px";
-			div.style.height = (this.field_height - h - 2) + "px";
-			div.style.borderStyle = "solid";
-			div.style.borderWidth = "1px";
-			div.style.position = "absolute";
-			div.style.borderColor = "darkgray";
+			var div = document.createElement('div');
+			div.id = 'b' + this.count;
+			div.style.left = (this.count * BOX_WIDTH) + 'px';
+			div.style.top = h + 'px';
+			div.style.width = (BOX_WIDTH - 4) + 'px';
+			div.style.height = (this.field_height - h - 2) + 'px';
+			div.style.borderStyle = 'solid';
+			div.style.borderWidth = '1px';
+			div.style.position = 'absolute';
+			div.style.borderColor = 'darkgray';
 			field.appendChild(div);
     		}
 
-		this.ship = document.createElement("div");
-		this.ship.id = "ship";
+		this.ship = document.createElement('div');
+		this.ship.id = 'ship';
 		this.ship.style.left = 0;
 		this.ship.style.top = 0;
-		this.ship.style.width = (BOX_WIDTH - 2) + "px";
-		this.ship.style.height = (BOX_HEIGHT) + "px";
-		this.ship.style.position = "absolute";
-		this.ship.style.backgroundColor = "#CD0403";
+		this.ship.style.width = (BOX_WIDTH - 2) + 'px';
+		this.ship.style.height = (BOX_HEIGHT) + 'px';
+		this.ship.style.position = 'absolute';
+		this.ship.style.backgroundColor = '#CD0403';
 		field.appendChild(this.ship);
 		this.dir = 1;
 
+		this.bomb = document.createElement('div');
+		this.bomb.id = 'bomb';
+		this.bomb.style.left = '0px';
+		this.bomb.style.top = '0px';
+		this.bomb.style.width = (BOX_WIDTH - 2) + 'px';
+		this.bomb.style.height = BOX_HEIGHT + 'px';
+		this.bomb.style.position = 'absolute';
+		this.bomb.style.backgroundColor = 'black';
+		this.bomb.style.display = 'none';
+		this.field.appendChild(this.bomb);
+	
 		this.animaton -= ANIMATION_DELTA;
 		if (this.animation < ANIMATION_MIN) {
 			this.animation = ANIMATION_MAX;
@@ -139,7 +150,7 @@ function Downtown(id_field, id_score, id_hi) {
 	}
 
 	this.shoot = function() {
-		if (this.bomb != null) return;
+		if (this.bomb.style.display == 'block') return;
 
 		if (this.paused) {
 			this.pauseToggle();
@@ -147,15 +158,9 @@ function Downtown(id_field, id_score, id_hi) {
 		else {	
 			left = Math.round(parseInt(this.ship.style.left) / BOX_WIDTH) * BOX_WIDTH;
 
-			this.bomb = document.createElement("div");
-			this.bomb.id = "bomb";
-			this.bomb.style.left = left + "px";
+			this.bomb.style.left = left + 'px';
 			this.bomb.style.top = this.ship.style.top;
-			this.bomb.style.width = (BOX_WIDTH - 2) + "px";
-			this.bomb.style.height = (BOX_HEIGHT) + "px";
-			this.bomb.style.position = "absolute";
-			this.bomb.style.backgroundColor = "black";
-			this.field.appendChild(this.bomb);
+			this.bomb.style.display = 'block';
 		}
 	}
 
@@ -163,14 +168,14 @@ function Downtown(id_field, id_score, id_hi) {
 		var left = parseInt(this.ship.style.left) + 2 * this.dir;
 		if (left == 0) {
 			this.dir = 1;
-			this.ship.style.top = (parseInt(this.ship.style.top) + BOX_HEIGHT) + "px";
+			this.ship.style.top = (parseInt(this.ship.style.top) + BOX_HEIGHT) + 'px';
 		}
 		else if (left + BOX_WIDTH >= this.field_width) {
 			this.dir = -1;
 		}
-		this.ship.style.left = left + "px";
+		this.ship.style.left = left + 'px';
 
-		var box = document.getElementById("b" + Math.floor(left / BOX_WIDTH));
+		var box = document.getElementById('b' + Math.floor(left / BOX_WIDTH));
 		if (parseInt(this.ship.style.top) + BOX_HEIGHT >= parseInt(box.style.top)) {
 			if (this.score > this.hi) {
 				this.text_hi.innerHTML = this.hi = this.score;
@@ -180,40 +185,40 @@ function Downtown(id_field, id_score, id_hi) {
 
 		if (this.bonus != null) {
 			this.bonus_count -= 1;
-			if (this.bomb == null && this.bonus_count < 1) {
-				this.bonus.style.backgroundColor = "white";
+			if (this.bomb.style.display != 'block' && this.bonus_count < 1) {
+				this.bonus.style.backgroundColor = 'white';
 				this.bonus = null;
 			}
 		}
 		else if (Math.random() < PCT_BONUS) {
 			var w = Math.floor(this.field_width / BOX_WIDTH);
 			do {
-				var id = "b" + Math.floor(Math.random() * w); 
+				var id = 'b' + Math.floor(Math.random() * w); 
 				this.bonus = document.getElementById(id);
 			}
-			while (this.bonus.style.display == "none")
-			this.bonus.style.backgroundColor = "darkgray";
+			while (this.bonus.style.display == 'none')
+			this.bonus.style.backgroundColor = 'darkgray';
 			this.bonus_count = w * BOX_WIDTH / 2;
 		}
 
-		if (this.bomb != null) { 
+		if (this.bomb.style.display == 'block') { 
 			var top = parseInt(this.bomb.style.top) + 2;
-			var id = "b" + Math.floor(parseInt(this.bomb.style.left) / BOX_WIDTH);
+
+			var id = 'b' + Math.floor(parseInt(this.bomb.style.left) / BOX_WIDTH);
 			var block = document.getElementById(id);
 			if (top > parseInt(block.style.top)) {
-				block.style.top = top + "px";
-				block.style.height = (this.field_height - top - 2) + "px";
+				block.style.top = top + 'px';
+				block.style.height = (this.field_height - top - 2) + 'px';
 			}
 
 			if (top + BOX_HEIGHT < this.field_height) {
-				this.bomb.style.top = top + "px";
+				this.bomb.style.top = top + 'px';
 			}
 			else {
-				this.bomb.parentNode.removeChild(this.bomb);
-				this.bomb = null;
+				this.bomb.style.display = 'none';
 
-				if (block.style.display != "none") {		
-					block.style.display = "none";
+				if (block.style.display != 'none') {		
+					block.style.display = 'none';
 
 					this.score += 1;
 					if (this.bonus != null && block.id == this.bonus.id) {
