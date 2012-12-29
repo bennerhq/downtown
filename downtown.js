7e15267370c7c283b498026502aa7c21906a69ef
Jens Kaas Benner <jens@bennerhq.com>
2012-12-29 14:42:22 +0100

    Update downtown.js

--- downtown.js
+++ downtown.js
@@ -4,8 +4,6 @@
  * <jens@bennerhq.com> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return. /benner
- *
- * Origin: http://people.freebsd.org/~phk/
  * ----------------------------------------------------------------------------
  */
 function Downtown(id_game, id_score, id_hi) {
